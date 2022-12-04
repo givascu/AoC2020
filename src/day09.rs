c9d40dca3c649d5c1056db7740593dae6c2c251d
Gabriel Ivașcu <ivascu.gabriel59@gmail.com>
2022-12-04 19:38:45 +0200

    Solve day 09

--- src/day09.rs
+++ src/day09.rs
@@ -1,7 +1,56 @@
+fn build_input_vector() -> Vec<i64> {
+    include_str!("../input/day09.txt")
+        .lines()
+        .map(|x| x.parse::<i64>().unwrap())
+        .collect::<Vec<_>>()
+}
+
 pub fn solve_1() -> i64 {
+    let v = build_input_vector();
+
+    let width = 25;
+    for k in width..v.len() {
+        let mut found = false;
+        for i in (k - width)..k {
+            if found {
+                break;
+            }
+            for j in (i + 1)..k {
+                if v[i] + v[j] == v[k] {
+                    found = true;
+                    break;
+                }
+            }
+        }
+        if !found {
+            return v[k];
+        }
+    }
+
+    -1
+}
+
+fn try_solve_2(v: &Vec<i64>, target: i64, width: usize) -> i64 {
+    for i in 0..=(v.len() - width) {
+        let slice = &v[i..i + width];
+        if slice.iter().sum::<i64>() == target {
+            return slice.iter().min().unwrap() + slice.iter().max().unwrap();
+        }
+    }
     -1
 }
 
 pub fn solve_2() -> i64 {
+    let target = solve_1();
+    assert!(target != -1);
+
+    let v = build_input_vector();
+    for width in 2..v.len() {
+        let res = try_solve_2(&v, target, width);
+        if res != -1 {
+            return res;
+        }
+    }
+
     -1
 }
