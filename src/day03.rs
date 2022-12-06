eb242fca2f4b5ef519a1d295bd4211e11556edb1
Gabriel Ivașcu <ivascu.gabriel59@gmail.com>
2022-12-06 08:53:21 +0200

    Day 03: Simplify implementation

--- src/day03.rs
+++ src/day03.rs
@@ -1,43 +1,14 @@
-use std::{
-    collections::HashMap,
-    ops::{Deref, DerefMut},
-};
+use std::collections::HashMap;
 
-#[derive(Clone, Copy, Debug, Default, Eq, PartialEq, Ord, PartialOrd, Hash)]
-struct Point2D {
-    y: usize,
-    x: usize,
-}
-
-impl Point2D {
-    fn new(y: usize, x: usize) -> Point2D {
-        Point2D { y, x }
-    }
-}
-
-struct Map2D<T> {
-    map: HashMap<Point2D, T>,
+struct Grid<T> {
+    map: HashMap<(usize, usize), T>,
     height: usize,
     width: usize,
 }
 
-impl<T> Deref for Map2D<T> {
-    type Target = HashMap<Point2D, T>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.map
-    }
-}
-
-impl<T> DerefMut for Map2D<T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.map
-    }
-}
-
-impl<T> Map2D<T> {
-    fn new(height: usize, width: usize) -> Map2D<T> {
-        Map2D {
+impl<T> Grid<T> {
+    fn new(height: usize, width: usize) -> Grid<T> {
+        Grid {
             map: HashMap::new(),
             height,
             width,
@@ -45,26 +16,26 @@ impl<T> Map2D<T> {
     }
 }
 
-fn build_input_map() -> Map2D<char> {
-    let mut map = Map2D::new(0, 0);
+fn build_input_map() -> Grid<char> {
+    let mut grid = Grid::new(0, 0);
     for (y, line) in include_str!("../input/day03.txt").lines().enumerate() {
         for (x, val) in line.chars().enumerate() {
-            map.insert(Point2D::new(y, x), val);
+            grid.map.insert((y, x), val);
         }
-        map.height += 1;
-        map.width = line.chars().count();
+        grid.height += 1;
+        grid.width = line.chars().count();
     }
-    map
+    grid
 }
 
-fn solve_slope(map: &Map2D<char>, dx: usize, dy: usize) -> i64 {
+fn solve_slope(grid: &Grid<char>, dx: usize, dy: usize) -> i64 {
     let (mut x, mut y) = (0, 0);
     let mut tree_count = 0;
 
-    while y < map.height - 1 {
+    while y < grid.height - 1 {
         y += dy;
-        x = (x + dx) % map.width;
-        if *map.get(&Point2D::new(y, x)).unwrap() == '#' {
+        x = (x + dx) % grid.width;
+        if *grid.map.get(&(y, x)).unwrap() == '#' {
             tree_count += 1;
         }
     }
@@ -73,15 +44,15 @@ fn solve_slope(map: &Map2D<char>, dx: usize, dy: usize) -> i64 {
 }
 
 pub fn solve_1() -> i64 {
-    let map = build_input_map();
-    solve_slope(&map, 3, 1)
+    let grid = build_input_map();
+    solve_slope(&grid, 3, 1)
 }
 
 pub fn solve_2() -> i64 {
-    let map = build_input_map();
-    solve_slope(&map, 1, 1)
-        * solve_slope(&map, 3, 1)
-        * solve_slope(&map, 5, 1)
-        * solve_slope(&map, 7, 1)
-        * solve_slope(&map, 1, 2)
+    let grid = build_input_map();
+    solve_slope(&grid, 1, 1)
+        * solve_slope(&grid, 3, 1)
+        * solve_slope(&grid, 5, 1)
+        * solve_slope(&grid, 7, 1)
+        * solve_slope(&grid, 1, 2)
 }
