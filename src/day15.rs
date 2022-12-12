0c523fda5d8df3a25d8bad494d5b0e839e4aaac2
Gabriel Ivașcu <ivascu.gabriel59@gmail.com>
2022-12-12 18:25:40 +0200

    Solve day 15

--- src/day15.rs
+++ src/day15.rs
@@ -1,7 +1,38 @@
-pub fn solve_1() -> i64 {
-    -1
+use std::collections::HashMap;
+
+fn solver(numbers: &[usize], target: usize) -> usize {
+    let mut cache = HashMap::new();
+    for i in 1..numbers.len() {
+        cache.insert(numbers[i - 1], i);
+    }
+
+    let mut last_num = *numbers.last().unwrap();
+    let mut turn = numbers.len();
+    while turn != target {
+        let new_num = match cache.get(&last_num) {
+            Some(val) => turn - *val,
+            None => 0,
+        };
+        cache.insert(last_num, turn);
+        last_num = new_num;
+        turn += 1;
+    }
+
+    last_num
 }
 
-pub fn solve_2() -> i64 {
-    -1
+pub fn solve_1() -> usize {
+    let numbers = include_str!("../input/day15.txt")
+        .split(',')
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+    solver(&numbers, 2020)
+}
+
+pub fn solve_2() -> usize {
+    let numbers = include_str!("../input/day15.txt")
+        .split(',')
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+    solver(&numbers, 30_000_000)
 }
